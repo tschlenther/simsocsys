3d82938a44784c825774c76553457424c46b8806
GrgrLmml <gregor.laemmel@gmail.com>
2016-04-17 11:48:29 +0200

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,32 @@
+/* *********************************************************************** *
+ * project: simsocsys
+ *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2016 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : gregor dot laemmel at gmail dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+
+
+/**
+ * Created by laemmel on 17/04/16.
+ */
+public class Hello {
+
+	public static void main(String [] args) {
+		System.out.println("Hello World!");
+	}
+
+}
